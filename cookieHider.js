677e4276064d9ae515e8ee841b3d57b82979968b
Smallpath <smallpath2013@gmail.com>
2016-03-13 13:47:57 +0800

    First Commit

---
+++ cookieHider.js
@@ -0,0 +1,99 @@
+// ==UserScript==
+// @name        Cookie Hider
+// @namespace   Smallpath2013@gmail.com
+// @description Hide cookies you don't like
+// @include     http://h.nimingban.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
+  this.GM_getValue=function (key,def) {
+      return localStorage[key] || def;
+  };
+  this.GM_setValue=function (key,value) {
+      return localStorage[key]=value;
+  };
+  this.GM_deleteValue=function (key) {
+      return delete localStorage[key];
+  };
+}
+Array.prototype._forEach = Array.prototype._forEach || function (callback, context) {
+  if (Object.prototype.toString.call(this) === '[object Array]') {
+    var i,
+    len;
+    for (i = 0, len = this.length; i < len; i++) {
+      if (typeof callback === 'function' && Object.prototype.hasOwnProperty.call(this, i)) {
+        if (callback.call(context, this[i], i, this) === false) {
+          break; // or return;
+        }
+      }
+    }
+  }
+}
+var version = 0.1;
+var blackList = [];
+
+
+try{
+  var blackListRes = GM_getValue('CookieHider','');
+  if(blackListRes != '')
+   blackList = blackListRes.split(',');
+  //console.log('blackList:'+blackList)
+}catch(err){console.log(err)}
+
+$("#h-tool").append('<a title="前往底部" id="edit-cookie" class="h-tool-btn">'
+                                  +'<i class="uk-icon-cog"></i></a>');
+
+$('#h-tool').on('click','#edit-cookie',function(){
+  
+  var str = prompt(blackList.toString());
+  
+  if(!str) return;
+  
+  if(str=='reset'){
+    blackList=[];
+    GM_setValue('CookieHider','');
+    return location.reload();;
+  }
+   
+  if(str.indexOf(/delete\s/)!=-1){
+    str = str.replace(/delete\s/g,'');
+    var newArr =[];
+    blackList.forEach(function(item){
+      if(item!=str)
+        newArr.push(item);
+    });
+    blackList = newArr;
+    GM_setValue('CookieHider',blackList.toString());
+    location.reload();
+    return;
+  }
+  
+  if(str){
+    var str = str.replace(/,|\s/g,'');
+    blackList.push(str);
+    if(blackListRes=='')
+     GM_setValue('CookieHider',str.toString());
+    else
+     GM_setValue('CookieHider',blackList.toString());
+    location.reload();
+    return;
+  }
+
+    
+});
+
+hideCookie(blackList);
+
+function hideCookie(blackArr) {
+  var targetElements = $.makeArray($('span.h-threads-info-uid'));
+  Array.prototype._forEach.call(targetElements, function (item, index) {
+    blackArr._forEach(function (item, index) {
+      var cookieName = this.innerHTML;
+      var resultIndex = cookieName.indexOf(item);
+      if (resultIndex != - 1) {
+        $(this).parents('div.h-threads-info').parent().parent().hide();
+      }
+    }, item);
+  })
+}
