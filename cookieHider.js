66a1fa6bb58dbe56e98b63440254895327f50338
Smallpath <smallpath2013@gmail.com>
2016-03-15 22:31:46 +0800

    Experience ES6 features

--- cookieHider.js
+++ cookieHider.js
@@ -6,15 +6,19 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
-  this.GM_getValue=function (key,def) {
-      return localStorage[key] || def;
+
+/************************
+*初始化油猴不支持的全局函数，并为数组原型添加新方法
+*************************/ 
+if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf('not supported') > - 1)) {
+  this.GM_getValue = function (key, def) {
+    return localStorage[key] || def;
   };
-  this.GM_setValue=function (key,value) {
-      return localStorage[key]=value;
+  this.GM_setValue = function (key, value) {
+    return localStorage[key] = value;
   };
-  this.GM_deleteValue=function (key) {
-      return delete localStorage[key];
+  this.GM_deleteValue = function (key) {
+    return delete localStorage[key];
   };
 }
 Array.prototype._forEach = Array.prototype._forEach || function (callback, context) {
@@ -30,80 +34,94 @@ Array.prototype._forEach = Array.prototype._forEach || function (callback, conte
     }
   }
 }
+Array.prototype._includes = Array.prototype._includes || function(str){
+  var i,
+      len;
+  for(i=0,len = this.length;i<len;i++)
+    if(this[i]==str)
+      return true;
+  return false;
+}
+
+/************************
+*读取黑名单
+*************************/ 
 var version = 0.1;
-var blackList = [];
+var blackList = new Array();
+var blackListRes = GM_getValue('CookieHider', '');
+if (blackListRes != '')
+  blackList = blackListRes.split(',');
 
 
-try{
-  var blackListRes = GM_getValue('CookieHider','');
-  if(blackListRes != '')
-   blackList = blackListRes.split(',');
-  //console.log('blackList:'+blackList)
-}catch(err){console.log(err)}
+/************************
+*添加屏蔽按钮
+*************************/ 
+$('span.h-threads-info-report-btn').each((index,item)=>{
+   if ($(item).find('a').text() == '举报')
+    $(item).append('<span class="h-threads-info-report-btn shield">[<a>屏蔽</a>]</span>');
+   });
 
-
-var targetElements = $.makeArray($('span.h-threads-info-report-btn'));
-  Array.prototype._forEach.call(targetElements, function (item, index) {
-    if($(item).find('a').text() == '举报')
-     $(item).append('<span class="h-threads-info-report-btn shield">[<a>屏蔽</a>]</span>');
-  });
-
-
-
-$('.shield').each(function(k,v){
-  $(v).find('a').click(function(e){
-      var item = $(this).parent().parent().parent();
-      //alert(item.parent().parent().next('hr').length.toString()+' '+(item.parent().attr('class') == 'h-threads-item-main').toString());
-      //if(item.parent().attr('class') == 'h-threads-item-main')
-      //  item.parent().parent().next('hr').hide();
-      var wow = item.children('.h-threads-info-uid');
-      blackList.push(wow.text().replace('ID:',''));
-      GM_setValue('CookieHider',blackList.toString());
-      hideCookie(blackList);
-  })
-})
-
-
-$("#h-tool").append('<a title="前往底部" id="edit-cookie" class="h-tool-btn">'
-                                  +'<i class="uk-icon-cog"></i></a>');
-
-$('#h-tool').on('click','#edit-cookie',function(){
-  
-  var str = prompt(blackList.toString());
-  
-  if(!str) return;
-  
-  if(str=='reset'){
-    blackList=[];
-    GM_setValue('CookieHider','');
-    return location.reload();
-  }
-   
-  if(str.indexOf('delete ')!=-1){
-    str = str.replace(/delete\s/g,'');
-    var newArr =[];
-    blackList.forEach(function(item){
-      if(item!=str)
-        newArr.push(item);
-    });
-    blackList = newArr;
-    GM_setValue('CookieHider',blackList.toString());
-    return location.reload();
-  }
+/************************
+*为屏蔽按钮绑定隐藏事件,注意：可能浏览器无法完美隐藏ｈｒ标签
+*************************/ 
+$('.shield').each(function (k, v) {
+  $(v).find('a').click(function(e) {
+    var item = $(this).parent().parent().parent();
     
+    if(item.parent().attr('class') == 'h-threads-item-main')
+      item.parent().parent().next('hr').hide();
+    
+    var wow = item.children('.h-threads-info-uid');
+    console.log(wow.text());
+    blackList.push(wow.text().replace('ID:', ''));
+    GM_setValue('CookieHider', blackList.toString());
+    hideCookie(blackList);
+  })
 });
 
+/************************
+*增加右部齿轮按钮，以提供黑名单移除与清空操作
+*************************/ 
+$('#h-tool').append('<a title="前往底部" id="edit-cookie" class="h-tool-btn"><i class="uk-icon-cog"></i></a>');
+
+/************************
+*为齿轮按钮绑定点击事件
+*************************/ 
+$('#h-tool').on('click', '#edit-cookie', function () {
+  var str = prompt(blackList.toString());
+  if (!str) return;
+  if (str == 'reset') {
+    blackList = [
+    ];
+    GM_setValue('CookieHider', '');
+    return location.reload();
+  }
+  if (str.indexOf('delete ') != - 1) {
+    str = str.replace(/delete\s/g, '');
+    var newArr = [
+    ];
+    blackList.forEach(function (item) {
+      if (item != str)
+      newArr.push(item);
+    });
+    blackList = newArr;
+    GM_setValue('CookieHider', blackList.toString());
+    return location.reload();
+  }
+});
+
+
+/************************
+*界面操作完毕，开始屏蔽
+*************************/ 
 hideCookie(blackList);
 
+/************************
+*屏蔽函数声明
+*************************/ 
 function hideCookie(blackArr) {
-  var targetElements = $.makeArray($('span.h-threads-info-uid'));
-  Array.prototype._forEach.call(targetElements, function (item, index) {
-    blackArr._forEach(function (item, index) {
-      var cookieName = this.innerHTML;
-      var resultIndex = cookieName.indexOf(item);
-      if (resultIndex != - 1) {
-        $(this).parents('div.h-threads-info').parent().parent().hide();
-      }
-    }, item);
-  })
+  $('.h-threads-info-uid').each((index,e)=>{
+    if (blackArr._includes($(e).text().replace('ID:','')))
+     $(e).parents('div.h-threads-info').parent().parent().css('display','none');
+   });
 }
